ed075e0767e001fb3a176e19687ecc5cfe356b3f
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2020-10-27 18:43:08 +0100

    simboli

--- src/show.js
+++ src/show.js
@@ -7,25 +7,30 @@ function showAllHotels() {
     let imgFile = document.createElement("img");
     let imgPath = hotel[i].images[0];
     let h2 = document.createElement("h2");
+    let stars = document.createElement("p");
     let bookBtn = document.createElement("button");
 
-    containerItems.setAttribute("id", "hotel-container");
     containerItems.setAttribute("class", "flex-container-items");
     imgFile.setAttribute("src", imgPath);
     imgFile.setAttribute("class", "show-images");
+    containerText.setAttribute("class", "container-text")
     h2.textContent = hotel[i].name;
-    h2.setAttribute("class", "hotel-name");
+    h2.setAttribute("class", "text");
+    stars.textContent = hotel[i].stars + " &#x2606";
+    stars.setAttribute("class", "text")
     bookBtn.textContent = "Book";
     bookBtn.setAttribute("class", "book-button");
 
     // containerText se sastoji od imena hotela, broja slobodnih soba, prosecne cene za jednu noc, dugmeta za rezervaciju...
     containerText.appendChild(h2);
+    containerText.appendChild(stars);
     containerText.appendChild(bookBtn);
 
     // containerItems se sastoji od dve komponente: imgFile i containerText
     containerItems.appendChild(imgFile);
     containerItems.appendChild(containerText);
-    document.body.appendChild(containerItems);
+    document.querySelector("#show-hotels").appendChild(containerItems)
+    document.body.appendChild(document.querySelector("#show-hotels"));
   }
 }
 
